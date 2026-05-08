96b8a41dbf2cd9e44d64f9f78456930260ca90bd
Cal Trew <calson57trew@users.noreply.github.com>
2026-05-09 02:32:09 +0800

    Add DEFAULT_TOP_K constant and fork note to package init

--- memsearch/__init__.py
+++ memsearch/__init__.py
@@ -2,6 +2,8 @@
 
 This package provides efficient vector similarity search capabilities
 with support for multiple distance metrics and index types.
+
+Note: Forked from zilliztech/memsearch for personal learning/experimentation.
 """
 
 from memsearch.index import Index
@@ -17,6 +19,9 @@ __version__ = "0.1.0"
 __author__ = "memsearch contributors"
 __license__ = "Apache-2.0"
 
+# Default number of results returned by search if top_k is not specified
+DEFAULT_TOP_K = 10
+
 __all__ = [
     "Index",
     "Collection",
@@ -24,5 +29,6 @@ __all__ = [
     "IndexNotFoundError",
     "DimensionMismatchError",
     "InvalidMetricError",
+    "DEFAULT_TOP_K",
     "__version__",
 ]
